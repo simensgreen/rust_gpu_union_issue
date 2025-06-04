15ce334db86c4a94dd8e967dcc91acb8f3ac8326
simens_green <simensgreen007@gmail.com>
2025-06-04 10:45:47 +0300

    next stage

--- build.rs
+++ build.rs
@@ -1,6 +1,8 @@
 use spirv_builder::SpirvBuilder;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    SpirvBuilder::new("shader", "spirv-unknown-spv1.3").build()?;
+    SpirvBuilder::new("shader", "spirv-unknown-spv1.3")
+        .print_metadata(spirv_builder::MetadataPrintout::Full)
+        .build()?;
     Ok(())
 }
--- shader/src/lib.rs
+++ shader/src/lib.rs
@@ -17,8 +17,24 @@ pub union DataOrArray {
 
 impl DataOrArray {
     pub fn arr(self) -> [f32; 5] { unsafe { self.arr } }
+    pub fn arr_ref(&self) -> &[f32; 5] { unsafe { &self.arr } }
     pub fn str(self) -> Data { unsafe { self.str } }
     pub fn new(arr: [f32; 5]) -> Self { Self { arr } }
+}
+
+#[derive(Clone, Copy)]
+pub union DataMatrix {
+    array1d: [f32; 25],
+    array2d: [[f32; 5]; 5],
+    data_array: [Data; 5],
+}
+
+impl DataMatrix {
+    pub fn array1d(self) -> [f32; 25] { unsafe { self.array1d } }
+    pub fn array2d(self) -> [[f32; 5]; 5] { unsafe { self.array2d } }
+    pub fn array2d_ref(&self) -> &[[f32; 5]; 5] { unsafe { &self.array2d } }
+    pub fn data_array(self) -> [Data; 5] { unsafe { self.data_array } } 
+    pub fn new(array1d: [f32; 25]) -> Self { Self { array1d } }
 }
 
 
@@ -26,8 +42,13 @@ impl DataOrArray {
 pub fn main(
     #[spirv(global_invocation_id)] UVec3 { x: id, .. }: UVec3,
     #[spirv(storage_buffer, binding=0)] input: &[DataOrArray],
+    #[spirv(storage_buffer, binding=0)] input_matrices: &[DataMatrix],
     #[spirv(storage_buffer, binding=1)] output: &mut [[f32; 5]],
 ) {
     let id = id as usize;
-    output[id] = input[id].arr();
+    // cannot cast between pointer types
+    output[id] = input_matrices[id].array2d_ref()[0];
+    // cannot memcpy dynamically sized data
+    // output[id] = input_matrices[id].array2d()[0];
+    output[id] = *input[id].arr_ref();
 }
